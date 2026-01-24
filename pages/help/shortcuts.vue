ee3a333a03adc567c316217d659b50a42418040f
steveasterafovo <steveasterafovo@gmail.com>
2026-01-24 23:22:09 +0100

    e

--- pages/help/shortcuts.vue
+++ pages/help/shortcuts.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-4xl mx-auto px-4 py-8 relative">
+  <div class="max-w-4xl mx-auto px-4 relative">
     <HelpShortcutsHeader @open-settings="showSettings = true" />
     <HelpShortcutsExpertMode />
 
